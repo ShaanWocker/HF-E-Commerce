0735ea39e70f7d41135db115f461a7cc73d900a3
ShaanWocker <ShaanWocker@gmail.com>
2021-11-05 23:09:02 +0200

    Search Icon, Search Input, Register, Sign In and Cart

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,88 @@
 import React from 'react';
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import styled from 'styled-components';
+import { Badge } from '@material-ui/core';
+
+const Container = styled.div`
+    height: 60px;
+`
+
+const Wrapper = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    align_items: center;
+    justify-content space-between;
+`
+
+const Left = styled.div`
+    flex:1;
+    display: flex;
+    align_items: center;
+`
+
+const Language = styled.span`
+    font-size: 14px;
+    cursor: pointer;
+`
+
+const SearchContainer = styled.div`
+    border: 0.5px solid lightgrey;
+    display: flex;
+    align_items: center;
+    margin-left: 25px;
+    padding: 5px
+`
+
+const Input = styled.input`
+    border: none;
+`
+
+const Center = styled.div`
+    flex:1;
+    text-align: center;
+`
+
+const Logo = styled.div`
+    font-weight: bold;
+`
+
+const Right = styled.div`
+    flex:1;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+`
+
+const MenuItem = styled.div`
+    font-size: 14px;
+    cursor: pointer;
+    margin-left: 25px;
+`
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
-        </div>
+        <Container>
+            <Wrapper>
+                <Left>
+                    <Language>EN</Language>
+                    <SearchContainer>
+                        <Input/>
+                        <Search style={{color:"gray", fontSize:16}}/>
+                    </SearchContainer>
+                </Left>
+                <Center><Logo>Honey Floral</Logo></Center>
+                <Right>
+                    
+                    <MenuItem>Register</MenuItem>
+                    <MenuItem>Sign In</MenuItem>
+                    <MenuItem>
+                        <Badge badgeContent={4} color="primary">
+                            <ShoppingCartOutlined />
+                        </Badge>
+                    </MenuItem>
+                </Right>
+            </Wrapper>
+        </Container>
     )
 }
 
