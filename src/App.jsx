18696ce6eb2a7e4fb50c720ed25d18ca2833749e
ShaanWocker <ShaanWocker@gmail.com>
2021-11-13 13:25:42 +0200

    Product-List

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
+// import Navbar from "./components/Navbar";
+// import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
 
 const App = () => {
-  return <Home/>;
+  return <ProductList/>;
 };
 
 export default App;
