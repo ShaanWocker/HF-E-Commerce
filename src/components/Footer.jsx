a079e813310c905b68cb0d1519e84b5a002b207d
ShaanWocker <ShaanWocker@gmail.com>
2021-11-13 10:53:48 +0200

    Footer, Useful Links and Contact info layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,4 @@
-// import { Instagram, Facebook, WhatsApp } from "material-ui/icons";
-import { Facebook, Instagram, WhatsApp } from "@material-ui/icons";
+import { Facebook, Instagram, WhatsApp, Room, Phone, MailOutlined } from "@material-ui/icons";
 import styled from "styled-components";
 
 
@@ -19,45 +18,121 @@ const Logo = styled.h1`
 `;
 
 const Desc = styled.p`
-
+    margin: 20px 0px;
 `;
 
 const SocialContainer = styled.div`
-
-`;
-
+    display: flex;
+`
 const SocialIcon = styled.div`
-
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    color: white;
+    background-color: #${props=>props.color};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 20px;
 `;
 
 const Center = styled.div`
     flex: 1;
+    padding: 20px;
+`;
+
+const Title = styled.h3`
+    margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+    width: 50%;
+    margin-bottom: 10px;
 `;
 
 const Right = styled.div`
     flex: 1;
+    padding: 20px;
+`;
+
+const ContactItem = styled.div`
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+`;
+
+const Payment = styled.img`
+    width: 50%;
 `;
 
 const Footer = () => {
     return (
         <Container>
             <Left>
-                <Logo></Logo>
+                <Logo>Honey Floral</Logo>
                 <Desc>I could say anything right over here</Desc>
                 <SocialContainer>
-                    <SocialIcon>
+                    <SocialIcon color="3B5999">
                         <Facebook/>
                     </SocialIcon>
-                    <SocialIcon>
+                    <SocialIcon color="E4405F">
                         <Instagram/>
                     </SocialIcon>
-                    <SocialIcon>
+                    <SocialIcon color="55ACEE">
                         <WhatsApp/>
                     </SocialIcon>
                 </SocialContainer>
             </Left>
-            <Center></Center>
-            <Right></Right>
+            <Center>
+
+                <Title>Useful Links</Title>
+                <List>
+                    <ListItem>Home</ListItem>
+                    <ListItem>Cart</ListItem>
+                    <ListItem>Mens</ListItem>
+                    <ListItem>Ladies</ListItem>
+                    <ListItem>Accessories</ListItem>
+                    <ListItem>My Account</ListItem>
+                    <ListItem>Order Tracking</ListItem>
+                    <ListItem>Wishlist</ListItem>
+                    <ListItem>Wishlist</ListItem>
+                    <ListItem>Terms</ListItem>
+                </List>
+            </Center>
+            <Right>
+                <Title>Contact</Title>
+                    <ContactItem>
+                        <Room style={{marginRight:"10px"}} />
+                            Shop 54B, 
+                            
+                            Brackenfell shopping center, 
+
+                            Corner of Jeanette street and 
+                            
+                            Old paarl road, 
+
+                            Brackenfell, 
+
+                            7560
+                    </ContactItem>
+                    <ContactItem>
+                        <Phone style={{marginRight:"10px"}}/>
+                          +27 66 250 1291 / +27 79 082 1181
+                    </ContactItem>
+                    <ContactItem>
+                        <MailOutlined style={{marginRight:"10px"}}/>
+                            GabrielleDuplessis97@gmail.com / ShaanWocker@gmail.com
+                    </ContactItem>
+                    <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+            </Right>
         </Container>
     );
 };
