79baf94146f21573cf9ebfba08031f86d0ee0f2d
Shaan Wocker <shaanwocker@gmail.com>
2022-03-03 22:43:11 +0200

    edits

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -75,7 +75,7 @@ const Navbar = () => {
                     <Language>EN</Language>
                     <SearchContainer>
                         <Input placeholder="Search"/>
-                        <Search style={{color: "gray", fontSize: "16"}}/>
+                        <Search style={{color: "gray", fontSize: 16}}/>
                     </SearchContainer>
                 </Left>
                 <Center><Logo>Honey Floral</Logo></Center>
