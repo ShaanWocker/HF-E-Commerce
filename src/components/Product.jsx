c0e451cf6165f9a525db319176e41989152df974
Shaan Wocker <ShaanWocker@gmail.com>
2025-07-08 18:19:16 +0200

    Implement dynamic stock handling and cart quantity validation

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,94 +1,108 @@
-import { FavoriteBorderOutlined, 
-    SearchOutlined, 
-    ShoppingCartOutlined 
-} from "@material-ui/icons";
-import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { ShoppingCartOutlined, SearchOutlined, FavoriteBorderOutlined } from "@material-ui/icons";
 
 const Info = styled.div`
-    opacity: 0;
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    top: 0;
-    left: 0;
-    background-color: rgba(0,0,0,0.2);
-    z-index: 3;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    transition: all 0.5s ease;
-    cursor: pointer;
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0,0,0,0.2);
+  z-index: 3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.5s ease;
+  cursor: pointer;
 `;
 
 const Container = styled.div`
-    flex: 1;
-    margin: 5px;
-    min-width: 280px;
-    height: 350px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: #f5fbfb;
-    position: relative;
+  flex: 1;
+  margin: 5px;
+  min-width: 280px;
+  height: 350px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f5fbfd;
+  position: relative;
 
-    &:hover ${Info}{
-        opacity: 1;
-    }
+  &:hover ${Info}{
+    opacity: 1;
+  }
+
+  opacity: ${(props) => (props.outOfStock ? 0.5 : 1)};
+  pointer-events: ${(props) => (props.outOfStock ? "none" : "auto")};
 `;
 
 const Circle = styled.div`
-    width: 200px;
-    height: 200px;
-    border-radius: 50%;
-    background-color: white;
-    position: absolute;
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  background-color: white;
+  position: absolute;
 `;
 
 const Image = styled.img`
-    height: 75% ;
-    z-index: 2;
+  height: 75%;
+  z-index: 2;
 `;
 
 const Icon = styled.div`
-    width: 40px;
-    height: 40px;
-    border-radius: 50%;
-    background-color: white;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin: 10px;
-    transition: all 0.5s ease;
-    &:hover {
-        background-color: #e9f5f5;
-        transform: scale(1.1);
-    }
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 10px;
+  transition: all 0.3s ease;
+
+  &:hover {
+    background-color: #e9f5f5;
+    transform: scale(1.1);
+  }
 `;
 
+const OutOfStockLabel = styled.div`
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  background-color: crimson;
+  color: white;
+  padding: 5px 10px;
+  font-size: 12px;
+  font-weight: bold;
+  border-radius: 3px;
+  z-index: 4;
+`;
 
-const Product = ({item}) => {
-    return (
-        <Container>
-            <Circle/>
-            <Image src={item.img}/>
-            <Info>
-                <Icon>
-                    <ShoppingCartOutlined/>
+const Product = ({ item }) => {
+  const isOutOfStock = item.inStock === 0;
 
-                </Icon>
-                <Icon>
-                    <Link to={`/product/${item._id}`}>
-                        <SearchOutlined/>
-                    </Link>
-                </Icon>
-                <Icon>
-                    <FavoriteBorderOutlined/>
-
-                </Icon>
-            </Info>
-        </Container>
-    );
+  return (
+    <Container outOfStock={isOutOfStock}>
+      {isOutOfStock && <OutOfStockLabel>OUT OF STOCK</OutOfStockLabel>}
+      <Circle />
+      <Image src={item.img} />
+      <Info>
+        <Icon>
+          <ShoppingCartOutlined />
+        </Icon>
+        <Icon>
+          <Link to={`/product/${item._id}`}>
+            <SearchOutlined />
+          </Link>
+        </Icon>
+        <Icon>
+          <FavoriteBorderOutlined />
+        </Icon>
+      </Info>
+    </Container>
+  );
 };
 
 export default Product;
