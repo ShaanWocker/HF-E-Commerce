fa706758dd8af1a53bfce36ebdc1199ed770792d
Shaan Wocker <ShaanWocker@gmail.com>
2025-07-15 13:23:34 +0200

    feat(media): add MediaManager image upload, delete & fetch fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,37 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Cart from './pages/Cart';
-import Login from './pages/Login';
-import Product from './pages/Product';
-import ProductList from './pages/ProductList';
-import Register from './pages/Register';
+import { useSelector } from "react-redux";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import ProductPage from "./pages/Product";
+import Cart from "./pages/Cart";
 import Success from "./pages/Success";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
-
-const App = () => {
+function App() {
+  const user = useSelector((s) => s.user.currentUser);
 
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products/:category" element={<ProductList />} />
-        <Route path="/product/:id" element={<Product />} />
+        <Route path="/product/:id" element={<ProductPage />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/success" element={<Success />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+
+        {/* Redirect logged‑in users away from login/register */}
+        <Route
+          path="/login"
+          element={user ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/register"
+          element={user ? <Navigate to="/" /> : <Register />}
+        />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
